bc4c7c793599f82e8b020c7e9c852a8eaea8bdbd
muzaffar401 <ma9400667@gmail.com>
2024-12-24 21:14:17 +0500

    new changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Product from "@/components/product";
 
 export default async function Home() {
-  const res = await fetch("/api/data");
+  const res = await fetch("https://api-product-fetch-git-main-muzaffar-ahmeds-projects.vercel.app//api/data");
   const data = await res.json();
 
   return (
