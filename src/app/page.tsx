ea31b0bd5123f93a62755359e1e9ade411e3b5d0
muzaffar401 <ma9400667@gmail.com>
2024-12-24 21:08:01 +0500

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Product from "@/components/product";
 
 export default async function Home() {
-  const res = await fetch("http://localhost:3000//api/data");
+  const res = await fetch("/api/data");
   const data = await res.json();
 
   return (
