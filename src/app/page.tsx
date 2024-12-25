c0242b8ad0eca6f45aeda295cfe7cc8a5b6a4caa
muzaffar401 <ma9400667@gmail.com>
2024-12-25 14:04:10 +0500

    comment project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Product from "@/components/product";
+// import Product from "@/components/product";
 
-export default async function Home() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/data`);
-  const data = await res.json();
+// export default async function Home() {
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/data`);
+//   const data = await res.json();
 
-  return (
-    <>
-      <Product data={data} />
-    </> 
-  );
-}
+//   return (
+//     <>
+//       <Product data={data} />
+//     </> 
+//   );
+// }
