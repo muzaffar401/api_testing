cfbc8bd150091d85a2295a784ec291a48e93a412
muzaffar401 <ma9400667@gmail.com>
2024-12-25 14:09:40 +0500

    new comment

--- src/helper/index.ts
+++ src/helper/index.ts
@@ -1,12 +1,12 @@
-// const getData = async () => {
-//   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/data`);
-//   const data = await res.json();
-//   return data;
-//   ;
-// }
+const getData = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/data`);
+  const data = await res.json();
+  return data;
+  ;
+}
 
-// export const detail = async (id: number) => {
-//   const item = await getData();
-//   const singleItem = await item.find((product: any) => product.id === id);
-//   return singleItem;
-// }
+export const detail = async (id: number) => {
+  const item = await getData();
+  const singleItem = await item.find((product: any) => product.id === id);
+  return singleItem;
+}
